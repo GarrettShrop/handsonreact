e99797c1bdcab548528fbd74e645aa025805d0c6
Virus <gshropshire@csumb.edu>
2023-05-26 00:28:54 +0100

    Added validation to form to make sure user enters proper values and added error popups if fields are filled out properly

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -22,7 +22,10 @@ function ProjectList({ projects, onSave }: ProjectListProps){
             {projects.map((project) => (
                 <div key={project.id} className="cols-sm">
                     {project === projectBeingEdited? (
-                        <ProjectForm onSave={onSave} onCancel={cancelEditing} />
+                        <ProjectForm 
+                        project={project}
+                        onSave={onSave} 
+                        onCancel={cancelEditing} />
                     ) : (
                         <ProjectCard project={project} onEdit={handleEdit} />
                     )}
